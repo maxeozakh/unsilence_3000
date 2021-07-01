2b580f3734b050b195d899f132ec8e65ee27e5f6
Maxim Zakharov <maxeozakh@gmail.com>
2021-07-01 21:28:38 +0300

    add gitgnore and entry point file

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+void streams_divider();
+
+int main() {
+    std::cout << "MAIN" << std::endl;
+    streams_divider();
+    std::cin.get();
+}
---
+++ src/streams_divider.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void streams_divider() {
+    std::cout << "STREAMS_DIVIDER INVOKED" << std::endl;
+}
