b94ba4f7735c8774dd2c70c70fd4a1d87a7c4210
Maxim Zakharov <maxeozakh@gmail.com>
2021-07-05 16:51:03 +0300

    cmake config with small test

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 void streams_divider();
 
 int main() {
-    std::cout << "MAIN" << std::endl;
+    streams_divider();
+    std::cout << "MAIN \n";
     streams_divider();
     std::cin.get();
 }
--- src/streams_divider.cpp
+++ src/streams_divider.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 
 void streams_divider() {
-    std::cout << "STREAMS_DIVIDER INVOKED" << std::endl;
+    std::cout << "STREAMS_DIVIDER INVOKED" << '\n';
 }
